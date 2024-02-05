f6958f4785fe4edf8dad9d8dbafdda60a7ac3f72
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2024-02-06 01:21:05 +0530

    Updated the store creation flow and implemented the relationship between store, settings and address entity

--- app/api/store/route.ts
+++ app/api/store/route.ts
@@ -4,29 +4,46 @@ import { NextResponse } from "next/server";
 
 export async function POST(
     req: Request,
-){
+) {
     try {
-        const {userId} = auth();
+        const { userId } = auth();
         const body = await req.json();
-        const {name,description} = body;
-        if(!userId){
-            return new NextResponse("Unauthorized",{status:401});
+        const { name, description, gstin, pan, addressLine1, adderessLine2, contactno, city, state, pincode } = body;
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }
-        if(!name){
-            return new NextResponse("Name is Required",{status:400});
+        if (!name) {
+            return new NextResponse("Name is Required", { status: 400 });
         }
         const store = await prismadb.store.create({
-            data:{
+            data: {
                 name,
                 description,
-                adminId:userId
+                adminId: userId
             }
         });
-
+        console.log(store)
+        const settings = await prismadb.settings.create({
+            data: {
+                gstin,
+                pan,
+                storeId: store.id
+            }
+        })
+        const address = await prismadb.address.create({
+            data: {
+                addressLine1: addressLine1 || "",
+                addressLine2: adderessLine2 || "",
+                contactno: contactno || "",
+                city: city || "",
+                state: state || "",
+                pincode: pincode || ""
+            }
+        })
         return NextResponse.json(store);
 
     } catch (error) {
-        console.log('[STORES_POST]',error);
-        return new NextResponse("Internal Error",{status:500});
+        console.log('[STORES_POST]', error);
+        return new NextResponse("Internal Error", { status: 500 });
     }
 }
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,7 +3,7 @@
 import { useForm } from "react-hook-form"
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
-import {toast} from "react-hot-toast"
+import { toast } from "react-hot-toast"
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
@@ -11,25 +11,43 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import axios from "axios";
+
 
 const formSchema = z.object({
     name: z.string().min(1),   // min length of 1 for the name of our store
-    description : z.string().min(1)
+    description: z.string().min(1),
+    gstin: z.string(),
+    pan: z.string(),
+    addressLine1: z.string(),
+    addressLine2: z.string(),
+    contactno: z.string(),
+    state: z.string(),
+    pincode: z.string(),
+    city: z.string(),
 })
 
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
 
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),   /// for out form to get validated by zod
         defaultValues: {
             name: "",
-            description :""
+            description: "",
+            gstin: "",
+            pan: "",
+            addressLine1: "",
+            addressLine2: "",
+            pincode: "",
+            state: "",
+            city: "",
+            contactno: ""
         },
     });
 
@@ -37,14 +55,14 @@ export const StoreModal = () => {
         try {
             setLoading(true);
 
-            const response = await axios.post("/api/store",values)
+            const response = await axios.post("/api/store", values)
             // console.log(response.data);
 
             toast.success("Store Created Successfully")
             window.location.assign(`/${response.data.id}`);
         } catch (error) {
             toast.error("SomeThing Went Wrong.")
-        }finally{
+        } finally {
             setLoading(false);
         }
     }
@@ -57,55 +75,222 @@ export const StoreModal = () => {
             onClose={storeModal.onClose}
         >
             <div>
+
                 <div className="space-y-4 py-2 pb-4">
                     <Form  {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FormField
-                                control={form.control}
-                                name="name"
-                                render={({field})=>(
-                                    <FormItem>
-                                        <FormLabel>
-                                            Name
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input          placeholder="E-commerce"
-                                            {...field}
-                                            disabled={loading}
-                                            />
-                                        </FormControl>
-                                        <FormMessage/>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="description"
-                                render={({field})=>(
-                                    <FormItem>
-                                        <FormLabel>
-                                            Description
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input          placeholder="Description"
-                                            {...field}
-                                            disabled={loading}
-                                            />
-                                        </FormControl>
-                                        <FormMessage/>
-                                    </FormItem>
-                                )}
-                            />
-                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button variant={"outline"}
-                                    disabled={loading}
-                                onClick={storeModal.onClose}>
-                                    Cancel
-                                </Button>
-                                <Button disabled={loading} type="submit" >
-                                    Continue
-                                </Button>
-                            </div>
+                            <Tabs defaultValue="account" className="w-[400px]">
+
+                                <TabsContent value="account">
+                                    <>
+                                        <FormField
+                                            control={form.control}
+                                            name="name"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Name
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="E-commerce"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="description"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Description
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Description"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </>
+                                </TabsContent>
+                                <TabsContent value="address">
+                                    <>
+                                        <FormField
+                                            control={form.control}
+                                            name="addressLine1"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Address line1
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="address"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="addressLine2"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Address line2
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="address"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="city"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        city
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="city"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="pincode"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Pin Code
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="pincode"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="state"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Pin Code
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="state"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="contactno"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Contact
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="contact"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </>
+                                </TabsContent>
+                                <TabsContent value="password">
+                                    <>
+                                        <FormField
+                                            control={form.control}
+                                            name="gstin"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        GST No
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="GST No."
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <FormField
+                                            control={form.control}
+                                            name="pan"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Pan Card No.
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="PAN No"
+                                                            {...field}
+                                                            disabled={loading}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                            <Button variant={"outline"}
+                                                disabled={loading}
+                                                onClick={storeModal.onClose}>
+                                                Cancel
+                                            </Button>
+                                            <Button disabled={loading} type="submit" >
+                                                Create
+                                            </Button>
+                                        </div>
+                                    </>
+                                </TabsContent>
+                                <TabsList className="justify-center items-center w-full bg-inherit mt-2">
+                                    <TabsTrigger value="account" >Basic</TabsTrigger>
+                                    <TabsTrigger value="address" >Address</TabsTrigger>
+                                    <TabsTrigger value="password">Verification</TabsTrigger>
+                                </TabsList>
+                            </Tabs>
+
+
 
 
                         </form>
