b483284c59758e97cc82c22e97bfec5e64d61841
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2024-02-08 02:05:42 +0530

    debbuged image-upload component

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,20 +1,77 @@
+"use client";
 import React from 'react'
+import { useState } from 'react'
+import * as z from "zod"
+import { set, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import ImageUpload from "@/components/ui/image-upload";
-import prismadb from '@/lib/prismadb'
+import toast from 'react-hot-toast'
 
-const page = async ({
-    params
-}:{params :{storeId : string , productId : string}}) => {
-    if(params.productId !== "new"){
-        const product = await prismadb.product.findUnique({where :{
-        id: params.productId ==="new"?"":params.productId ,
-        storeId : params.storeId
-    }})
+const formSchema = z.object({
+    imageUrl: z.string()
+})
+
+type imageFromValues = z.infer<typeof formSchema>
+
+interface settingsFormProps {
+    initialData: "" | null
+}
+
+
+const page: React.FC<settingsFormProps> = ({
+    initialData
+}) => {
+    const [loading , setLoading] = useState(false)
+
+    const form = useForm<imageFromValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {
+            imageUrl: ""
+        }
+    })
+
+    const onSubmit = async (data: imageFromValues) => {
+        try {
+            setLoading(true);
+            // if(initialData){
+                console.log(data)
+            //     await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+            // }else{
+            //     await axios.post(`/api/${params.storeId}/billboards`, data);
+            // }
+            // router.refresh();
+            // router.push(`/${params.storeId}/billboards`)
+            toast.success("Hl")
+        } catch (error) {
+            toast.error("Something Went Wrong");
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
         <div>
-
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Background Image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload value={field.value ? [field.value] : []}
+                                        disabled={loading} onChange={(url) => field.onChange(url)}
+                                        onRemove={() => field.onChange("")}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
         </div>
     )
 }
